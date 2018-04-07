74a4dd624f0e19b2464b0375ab5f169e2384918e
nibreining <nibreining@dagdasolutions.de>
2018-04-07 12:51:13 +0200

    Layout for tablet added

--- app/src/main/java/com/selfhack/devprofile/MainActivity.kt
+++ app/src/main/java/com/selfhack/devprofile/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.selfhack.devprofile
 
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bitmap = BitmapFactory.decodeResource(resources,R.drawable.devslopesprofilelogo)
+        val rouded = RoundedBitmapDrawableFactory.create(resources,bitmap)
+        rouded.cornerRadius = 15f
+        logo.setImageDrawable(rouded)
     }
 }
